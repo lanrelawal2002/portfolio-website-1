06a09fd46b76fe18c886c4c3da3aaf441d6ada41
lanrelawal0022 <lanrelawal2002@gmail.com>
2025-08-22 14:19:38 +0100

    starting files for portfolio project

---
+++ script.js
@@ -0,0 +1,5 @@
+const footerYear = document.getElementById("current-year");
+
+footerYear.textContent = new Date().getFullYear();
+
+// document.getElementById("current-year").textContent = new Date().getFullYear();
